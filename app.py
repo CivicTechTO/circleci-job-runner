f08a18b43cccc412685d5159d0384f089634c533
Patrick Connolly <patrick.c.connolly@gmail.com>
2019-04-16 11:06:42 -0400

    Added /jobs endpoint for listing jobs, incl blacklist.

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, request
+import json
 import os
 import requests
+import yaml
 
 
 CIRCLE_API_TOKEN = os.environ.get('CIRCLE_API_USER_TOKEN', '')
 BRANCH = os.environ.get('GIT_BRANCH', '')
 REPO = os.environ.get('GITHUB_REPO', '')
+JOBS_BLACKLIST = os.environ.get('JOBS_BLACKLIST', 'default').split(',')
 
 app = Flask(__name__)
 
@@ -15,6 +18,13 @@ if not REPO or not CIRCLE_API_TOKEN:
 @app.route('/')
 def home():
     return('Hello World!')
+
+@app.route('/jobs', methods=['GET'])
+def list_jobs():
+    if request.method == 'GET':
+        return get_job_names()
+    else:
+        return 'Not found', 404
 
 @app.route('/jobs/<job_name>', methods=['POST'])
 def do_job_action(job_name=None):
@@ -26,6 +36,15 @@ def do_job_action(job_name=None):
 def get_default_branch():
     r = requests.get('https://api.github.com/repos/' + REPO)
     return r.json()['default_branch']
+
+def get_job_names():
+    branch = get_default_branch() if not BRANCH else BRANCH
+    config_url = 'https://raw.githubusercontent.com/{}/{}/.circleci/config.yml'.format(REPO, branch)
+    r = requests.get(config_url)
+    config = yaml.safe_load(r.text)
+    job_names = list(config['jobs'].keys())
+    job_names = [j for j in job_names if j not in JOBS_BLACKLIST]
+    return json.dumps(job_names)
 
 def run_job(job_name=None):
     payload = {
